4fa5f9c45d9b3d44ac14f21535b9a77e4a08d60e
Catherine Sangiovanni <goldyflakes@gmail.com>
2025-08-05 17:09:04 -0400

    generate random message id

--- server/Lib.cs
+++ server/Lib.cs
@@ -46,17 +46,30 @@ public static partial class Module
 
         return name;
     }
+    
+    static Random random = new Random();
+    public static string GetRandomHexNumber(int digits)
+    {
+        byte[] buffer = new byte[digits / 2];
+        random.NextBytes(buffer);
+        string result = String.Concat(buffer.Select(x => x.ToString("X2")).ToArray());
+        if (digits % 2 == 0)
+            return result;
+        return result + random.Next(16).ToString("X");
+    }
 
     [Reducer]
     public static void SendMessage(ReducerContext ctx, string text)
     {
         text = ValidateMessage(text);
-        Log.Info($"{ctx.Sender.ToString()}: {text}");
+        Identity id = Identity.FromHexString(GetRandomHexNumber(64));
+        Log.Info($"({id}). {ctx.Sender.ToString()}: {text}");
         ctx.Db.message.Insert(new Message
         {
             Sender = ctx.Sender,
             Text = text,
-            Sent = ctx.Timestamp
+            Sent = ctx.Timestamp,
+            Id = id
         });
     }
 
